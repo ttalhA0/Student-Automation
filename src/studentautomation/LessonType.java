436be96e2e2a9b4d28a3befabcbb4d6eef792b34
Talha Tanrıverdi <tanriverdiitalha@gmail.com>
2025-02-25 21:09:59 +0300

    new-features: Last LessonType upgrade fixed and getter methods added

--- src/studentautomation/LessonType.java
+++ src/studentautomation/LessonType.java
@@ -1,24 +1,20 @@
 package studentautomation;
 
+import lombok.Getter;
+
 public enum LessonType {
     FIZIK ("FİZİK", "PHYS121"),
     MATEMATIK ("MATEMATİK", "MATH101"),
     TURKCE ("TÜRKÇE", "TUR101");
 
+    @Getter
     private String title;
+    @Getter
     private String classID;
 
     LessonType(String title, String classID) {
         this.title = title;
         this.classID = classID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getClassID() {
-        return classID;
     }
 }
 
